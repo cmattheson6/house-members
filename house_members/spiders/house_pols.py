d820ac4fd9240def6295e281952efca97860f4b9
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-08-30 19:56:08 -0400

    Update house_pols.py
    There are certain SQL-based mappings that have been temporarily commented out. These need to be added back in to the Dataflow data cleaning process.

--- house_members/spiders/house_pols.py
+++ house_members/spiders/house_pols.py
@@ -5,7 +5,7 @@ Created on May 29, 2018
 '''
 
 import scrapy
-import psycopg2
+# import psycopg2
 
 class HousePolsSpider(scrapy.Spider):
     name = "house_pols"
@@ -15,23 +15,23 @@ class HousePolsSpider(scrapy.Spider):
         yield scrapy.Request(url = start_url, callback = self.parse)
     
     def parse(self, response):
-        hostname = 'localhost'
-        username = 'postgres'
-        password = 'postgres'
-        database = 'politics'
-        self.conn = psycopg2.connect(
-            host = hostname,
-            user = username,
-            password = password,
-            dbname = database)
-        self.cur = self.conn.cursor()
-        map_query = """select state, initials from state_map"""
-        self.cur.execute(map_query)
-        mapping_list = list(self.cur)
-        self.cur.close()
-        self.conn.close()
-        state_list = [i[0] for i in mapping_list]
-        initials_list = [i[1] for i in mapping_list]
+#         hostname = 'localhost'
+#         username = 'postgres'
+#         password = 'postgres'
+#         database = 'politics'
+#         self.conn = psycopg2.connect(
+#             host = hostname,
+#             user = username,
+#             password = password,
+#             dbname = database)
+#         self.cur = self.conn.cursor()
+#         map_query = """select state, initials from state_map"""
+#         self.cur.execute(map_query)
+#         mapping_list = list(self.cur)
+#         self.cur.close()
+#         self.conn.close()
+#         state_list = [i[0] for i in mapping_list]
+#         initials_list = [i[1] for i in mapping_list]
         
         # finds the path to the table that contains a single state's representatives
         for region in response.xpath("//table[@class='table']"):
@@ -42,9 +42,9 @@ class HousePolsSpider(scrapy.Spider):
                 pass
             # pulls the state of all representatives in this group; will be pulled into dictionary
             # also maps from full state to state initial
-            state_raw = region.xpath(".//caption/text()").extract_first()
-            state_processed = state_raw.strip(' \t\n\r')
-            state = initials_list[state_list.index(state_processed)]
+            state = region.xpath(".//caption/text()").extract_first()
+            state = state_raw.strip(' \t\n\r')
+#             state = initials_list[state_list.index(state_processed)]
             
             # establishes the path to each representative of the one state
             for rep in region.xpath(".//tbody/tr"):
@@ -52,22 +52,22 @@ class HousePolsSpider(scrapy.Spider):
                 
                 # pulls the first name of the representative
                 if "," in full_name:
-                    first_name_raw = full_name.split(',')[1]
+                    first_name = full_name.split(',')[1]
                 else:
-                    full_name_revised = full_name.replace(" ", ",", 1)
-                    first_name_raw = full_name_revised.split(',')[1]
+                    full_name = full_name.replace(" ", ",", 1)
+                    first_name = full_name_revised.split(',')[1]
                 first_name = first_name_raw.strip(' \t\n\r')
                 
                 # pulls the last name of the representative
-                last_name_raw = full_name.split(',')[0]
+                last_name = full_name.split(',')[0]
                 last_name = last_name_raw.strip(' \t\n\r')
                 
                 # pulls the party of the given representative
-                party_raw = rep.xpath(".//td/text()").extract()[2]
-                party = party_raw.strip(' \tn\r')
+                party = rep.xpath(".//td/text()").extract()[2]
+                party = party_raw.strip(' \t\n\r')
                 
                 # pulls the district of the given representative
-                district_raw = rep.xpath(".//td/text()").extract()[0]
+                district = rep.xpath(".//td/text()").extract()[0]
                 district = district_raw.strip(' \t\n\r')
                 
                 # combines all data pieces of a representative into a dictionary
