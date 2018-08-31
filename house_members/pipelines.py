381f6594f3979f5a513b10d3691ea76a0233b120
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-08-30 20:00:46 -0400

    Update pipelines.py
    Certain SQL functions have been commented out due to the changing in code. Some functions will need to be moved to Dataflow, and some parts will need to be replaced by a Pub/Sub topic publication script.

--- house_members/pipelines.py
+++ house_members/pipelines.py
@@ -6,7 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 # import necessary modules
-import psycopg2
+# import psycopg2
 import datetime
 from datetime import date
 import time
@@ -15,88 +15,88 @@ import time
 date_today = date.today()
 
 class PoliticiansPipeline(object):
-     def open_spider(self, spider):
-         hostname = 'localhost'
-         username = 'postgres'
-         password = 'postgres'
-         database = 'politics'
-         self.conn = psycopg2.connect(
-             host = hostname,
-             user = username,
-             password = password,
-             dbname = database)
-         self.cur = self.conn.cursor()
+#      def open_spider(self, spider):
+#          hostname = 'localhost'
+#          username = 'postgres'
+#          password = 'postgres'
+#          database = 'politics'
+#          self.conn = psycopg2.connect(
+#              host = hostname,
+#              user = username,
+#              password = password,
+#              dbname = database)
+#          self.cur = self.conn.cursor()
             
-     def close_spider(self, spider):
-         self.cur.close()
-         self.conn.close()
+#      def close_spider(self, spider):
+#          self.cur.close()
+#          self.conn.close()
             
-     def process_item(self, item, spider):
-         select_query = """select first_name, last_name, party, state from politicians"""
-         self.cur.execute(select_query)
-         politicians_list = list(self.cur)
-         pol_check_tuple = (item['first_name'], item['last_name'], item['party'], item['state'])
+#      def process_item(self, item, spider):
+#          select_query = """select first_name, last_name, party, state from politicians"""
+#          self.cur.execute(select_query)
+#          politicians_list = list(self.cur)
+#          pol_check_tuple = (item['first_name'], item['last_name'], item['party'], item['state'])
             
-         insert_query = """insert into politicians (first_name, last_name, party, state)
-             values (%s, %s, %s, %s)"""
-         pol_packet = (item['first_name'], item['last_name'], item['party'], item['state'])
+#          insert_query = """insert into politicians (first_name, last_name, party, state)
+#              values (%s, %s, %s, %s)"""
+#          pol_packet = (item['first_name'], item['last_name'], item['party'], item['state'])
                     
-         if pol_check_tuple in politicians_list:
-             return item
-         else:
-             self.cur.execute(insert_query, vars = pol_packet)
-             self.conn.commit()
-             return item
+#          if pol_check_tuple in politicians_list:
+#              return item
+#          else:
+#              self.cur.execute(insert_query, vars = pol_packet)
+#              self.conn.commit()
+#              return item
 
 class HouseMembersPipeline(object):
-    def open_spider(self, spider):
-        hostname = 'localhost'
-        username = 'postgres'
-        password = 'postgres'
-        database = 'politics'
-        self.conn = psycopg2.connect(
-            host = hostname,
-            user = username,
-            password = password,
-            dbname = database)
-        self.cur = self.conn.cursor()
+#     def open_spider(self, spider):
+#         hostname = 'localhost'
+#         username = 'postgres'
+#         password = 'postgres'
+#         database = 'politics'
+#         self.conn = psycopg2.connect(
+#             host = hostname,
+#             user = username,
+#             password = password,
+#             dbname = database)
+#         self.cur = self.conn.cursor()
     
-    def close_spider(self, spider):
-        self.cur.close()
-        self.conn.close()
+#     def close_spider(self, spider):
+#         self.cur.close()
+#         self.conn.close()
    
     def process_item(self, item, spider):
-        house_member = ()
+#         house_member = ()
         
-        select_query = """select first_name, last_name, party, state from house"""
-        self.cur.execute(select_query)
-        rep_tuple_list = list(self.cur)
+#         select_query = """select first_name, last_name, party, state from house"""
+#         self.cur.execute(select_query)
+#         rep_tuple_list = list(self.cur)
          
-        select_query = """select first_name, last_name, party, state from politicians"""
-        self.cur.execute(select_query)
-        pol_tuple_list = list(self.cur)
+#         select_query = """select first_name, last_name, party, state from politicians"""
+#         self.cur.execute(select_query)
+#         pol_tuple_list = list(self.cur)
             
-        select_query = """select * from house"""
-        self.cur.execute(select_query)
-        all_reps_list = list(self.cur) # eventually change this to Pandas data frame
+#         select_query = """select * from house"""
+#         self.cur.execute(select_query)
+#         all_reps_list = list(self.cur) # eventually change this to Pandas data frame
             
-        select_query = """select * from politicians"""
-        self.cur.execute(select_query)
-        all_pols_list = list(self.cur) # eventually change this to Pandas data frame
+#         select_query = """select * from politicians"""
+#         self.cur.execute(select_query)
+#         all_pols_list = list(self.cur) # eventually change this to Pandas data frame
             
-        select_all_query = """select id from house"""
-        self.cur.execute(select_query)
-        house_reps_list = list(self.cur) # eventually change this to Pandas data frame
+#         select_all_query = """select id from house"""
+#         self.cur.execute(select_query)
+#         house_reps_list = list(self.cur) # eventually change this to Pandas data frame
           
 
         pol_tuple = (item['first_name'], item['last_name'], item['party'], item['state'])
         
-        insert_query = """insert into house (id, first_name, last_name, party, 
-            state, district, start_date, end_date, tenure_num)
-            values (%s, %s, %s, %s, %s, %s, %s, %s, %s)""" 
-        update_query = """update house
-                     set end_date = %s
-                     where id = %s"""
+#         insert_query = """insert into house (id, first_name, last_name, party, 
+#             state, district, start_date, end_date, tenure_num)
+#             values (%s, %s, %s, %s, %s, %s, %s, %s, %s)""" 
+#         update_query = """update house
+#                      set end_date = %s
+#                      where id = %s"""
       
         pol_id = all_pols_list[pol_tuple_list.index(pol_tuple)][0]
         house_pol_packet = (pol_id, 
@@ -114,19 +114,19 @@ class HouseMembersPipeline(object):
             and date_today - all_reps_list[rep_tuple_list.index(pol_tuple)][7] 
             # the previous line needs to change as it will not be functional past two iterations
             <= datetime.timedelta(days = 30)):
-            self.cur.execute(update_query, vars = [date_today, pol_id])
-            self.conn.commit()
+#             self.cur.execute(update_query, vars = [date_today, pol_id])
+#             self.conn.commit()
             return item
         elif (pol_tuple in rep_tuple_list 
             and date_today - all_reps_list[rep_tuple_list.index(pol_tuple)][7] 
             # the previous line needs to change as it will not be functional past two iterations
             > datetime.timedelta(days = 30)):
-            self.cur.execute(insert_query, vars = house_pol_packet)
-            self.conn.commit()
+#             self.cur.execute(insert_query, vars = house_pol_packet)
+#             self.conn.commit()
             return item
         else:
-            self.cur.execute(insert_query, vars = house_pol_packet)
-            self.conn.commit()
+#             self.cur.execute(insert_query, vars = house_pol_packet)
+#             self.conn.commit()
             return item
         
         # This is the trial code from the separate pipeline
