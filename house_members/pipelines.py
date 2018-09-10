b3b5c23755a6a31a381ab58a779b074a37828a69
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-09-10 08:46:18 -0400

    Update pipelines.py

--- house_members/pipelines.py
+++ house_members/pipelines.py
@@ -18,25 +18,25 @@ date_today = date.today()
 
 class PoliticiansPipeline(object):
      def open_spider(self, spider):
-#             settings = crawler.settings
-#             credentials = {
-#                         'auth_provider_x509_cert_url': settings.get('auth_provider_x509_cert_url'),
-#                         'auth_uri': settings.get('auth_uri'),
-#                         'client_email': settings.get('client_email'),
-#                         'client_id': settings.get('client_id'),
-#                         'client_x509_cert_url': settings.get('client_x509_cert_url'),
-#                         'private_key': settings.get('private_key'),
-#                         'private_key_id': settings.get('private_key_id'),
-#                         'project_id': settings.get('project_id'),
-#                         'token_uri': settings.get('token_uri'),
-#                         'type': settings.get('account_type')
-#             }
-#             print(self.CREDENTIALS)
+            settings = crawler.settings
+            credentials = {
+                        'auth_provider_x509_cert_url': settings.get('auth_provider_x509_cert_url'),
+                        'auth_uri': settings.get('auth_uri'),
+                        'client_email': settings.get('client_email'),
+                        'client_id': settings.get('client_id'),
+                        'client_x509_cert_url': settings.get('client_x509_cert_url'),
+                        'private_key': settings.get('private_key'),
+                        'private_key_id': settings.get('private_key_id'),
+                        'project_id': settings.get('project_id'),
+                        'token_uri': settings.get('token_uri'),
+                        'type': settings.get('account_type')
+            }
+            print(self.CREDENTIALS)
 #             publisher = pubsub.PublisherClient.from_service_account_json(self.credentials)
 #             topic = 'projects/{project_id}/topics/{topic}'.format(
 #                  project_id='politics-data-tracker-1',
 #                  topic='house_members')
-            pass
+#             pass
 #          hostname = 'localhost'
 #          username = 'postgres'
 #          password = 'postgres'
